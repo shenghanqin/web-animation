bcd00cb10d4a7b5dc79a8c57b44d49ef9f8a3bd1
shenghanqin <shenghanqin@hujiang.com>
2019-04-20 23:02:27 +0800

    更新path动画效果

--- src/logo-loading-path/index.js
+++ src/logo-loading-path/index.js
@@ -9,26 +9,28 @@ const cx = classnames.bind(styles)
 const prefixClass = `${PREFIX}-cclogo-loading`
 
 
+const SKIN = {
+  blue: '#2F5AFF',
+  white: '#fff'
+}
+
+
+
 export default class LogoLoading extends Component {
-  constructor(props) {
-    super(props)
-  }
 
   render() {
     let { skin, style } = this.props
+    let iconColor = SKIN[skin] || skin
     return (
-      <div className={cx(`${prefixClass}`, { [`${prefixClass}-white`]: skin !== 'blue' })} style={style}>
+      <div className={cx(`${prefixClass}`)} style={style}>
         <svg className={cx(`${prefixClass}-svg`)} version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600">
           <mask id={`${prefixClass}-mask`}>
             <circle cx="300" cy="300" r="300" fill="white" />
             <rect x="0" y="277" width="600" height="46" fill="black" />
           </mask>
-          <g>
-            <path className={cx(`${prefixClass}-ring ${prefixClass}-ring-1`)} fill="none" stroke="#008CED" d="M50,300 A290,290, 0,0,1, 630,300 M630,300 A290,290, 0,0,1, 50,300" strokeWidth="120" mask={`url(#${prefixClass}-mask)`} />
-            {/* <path className={cx(`${prefixClass}-ring ${prefixClass}-ring-2`)} fill="none" stroke="#008CED" d="" strokeWidth="120" mask={`url(#${prefixClass}-mask)`} /> */}
-          </g>
-          <path className={cx(`${prefixClass}-eye`)} d="M279.185,251.568s-.647-25.358,15.574-7.6,13.709,44.548,11.418,52.7-10.629,11.759-17.006,4.378C291.83,277.634,279.185,251.568,279.185,251.568Z" fill="#008bec" />
-          <path className={cx(`${prefixClass}-eye`)} d="M429.185,221.568s-.647-25.358,15.574-7.6,13.709,44.548,11.418,52.7-10.629,11.759-17.006,4.378C441.83,247.634,429.185,221.568,429.185,221.568Z" fill="#008bec" />
+          <path className={cx(`${prefixClass}-ring ${prefixClass}-ring-1`)} fill="none" stroke={iconColor} d="M50 300 A290,290, 0,1,1, 50.000001858166243,300.02129301676624" strokeWidth="100" mask={`url(#${prefixClass}-mask)`} />
+          <path className={cx(`${prefixClass}-eye`)} d="M279.185,251.568s-.647-25.358,15.574-7.6,13.709,44.548,11.418,52.7-10.629,11.759-17.006,4.378C291.83,277.634,279.185,251.568,279.185,251.568Z" fill={iconColor} />
+          <path className={cx(`${prefixClass}-eye`)} d="M429.185,221.568s-.647-25.358,15.574-7.6,13.709,44.548,11.418,52.7-10.629,11.759-17.006,4.378C441.83,247.634,429.185,221.568,429.185,221.568Z" fill={iconColor} />
         </svg>
       </div>
     )
