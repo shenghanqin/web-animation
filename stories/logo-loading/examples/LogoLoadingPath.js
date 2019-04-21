fe5f6bc692f85369f1d017b885012e1b96aef46f
shenghanqin <shenghanqin@hujiang.com>
2019-04-21 21:26:55 +0800

    重新命名，添加新动画

--- stories/logo-loading/examples/LogoLoadingPath.js
+++ stories/logo-loading/examples/LogoLoadingPath.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 
 import LogoLoadingRotate from '@components/logo-loading-1-rotate'
 import LogoLoadingPath from '@components/logo-loading-2-path'
-import LogoLoadingEye from '@components/logo-loading-path-eye'
+import LogoLoadingEye from '@components/logo-loading-3-path-eye'
+import LogoLoadingRing from '@components/logo-loading-4-circle-ring'
 
 export default class BackTopExample extends Component {
 
@@ -15,11 +16,13 @@ export default class BackTopExample extends Component {
               <th style={{ padding: '0 20px' }}><h3>V1 旋转动画</h3></th>
               <th style={{ padding: '0 20px' }}><h3>V2 圆圈轨迹动画</h3></th>
               <th style={{ padding: '0 20px' }}><h3>V3 圆圈加眼睛动画</h3></th>
+              <th style={{ padding: '0 20px' }}><h3>V4 简化动画</h3></th>
             </tr>
             <tr>
               <td><LogoLoadingRotate {...this.props} /></td>
               <td><LogoLoadingPath {...this.props} /></td>
               <td><LogoLoadingEye {...this.props} /></td>
+              <td><LogoLoadingRing {...this.props} /></td>
             </tr>
           </table>
         </React.StrictMode>
