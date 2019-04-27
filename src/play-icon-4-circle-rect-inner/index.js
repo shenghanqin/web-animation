888398aacad182ab3cedede2f5e22b3ca6649567
shenghanqin <shenghanqin@163.com>
2019-04-27 19:15:50 +0800

    提交一版本正方向3

--- src/play-icon-4-circle-rect-inner/index.js
+++ src/play-icon-4-circle-rect-inner/index.js
@@ -40,6 +40,91 @@ export default class LogoLoading extends Component {
     })
   }
 
+  renderToPause = () => {
+    let { skin, style } = this.props
+    let iconColor = SKIN[skin] || skin
+    return (
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120" className={cx(`${audioClass}-btn`)}>
+        <g className={cx(`${audioClass}-group`)} transform="translate(19, 0)">
+          <animateTransform attributeType="XML"
+            attributeName="transform" type="translate"
+            values="0 0; 19 0; 19 0"
+            dur={DURATION_TIME} />
+          <g>
+            <mask id={`${audioClass}-mask`}>
+              <polygon fill="white" points="0,0 103.4,60 0,120" />
+            </mask>
+            <circle className={cx(`${audioClass}-ring`)} cx="34.64" cy="60" r="25.7" fill={iconColor} mask={`url(#${audioClass}-mask)`}>
+              <animate attributeType="XML"
+                attributeName="r"
+                values="69.2; 25.7; 25.7"
+                dur={DURATION_TIME} />
+            </circle>
+
+          </g>
+        </g>
+        <rect className={cx(`${audioClass}-eye`)} x="0" y="8.6" rx="15" ry="15" width="103.4" height="103.4" fill={iconColor} transform="scale(1)">
+          <animate attributeType="XML"
+            attributeName="rx"
+            values="51.7;51.7;15"
+            dur={DURATION_TIME} />
+          <animate attributeType="XML"
+            attributeName="ry"
+            values="51.7;51.7;15"
+            dur={DURATION_TIME} />
+          <animateTransform attributeType="XML"
+            attributeName="transform" type="scale"
+            values="0.3; 0.5; 1"
+            dur={DURATION_TIME} />
+        </rect>
+      </svg>
+    )
+  }
+
+  // 反向
+  renderToPlay = () => {
+    let { skin, style } = this.props
+    let iconColor = SKIN[skin] || skin
+    return (
+      <div>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120" className={cx(`${audioClass}-btn`)}>
+          <g className={cx(`${audioClass}-group`)} transform="translate(0, 0)">
+            <animateTransform attributeType="XML"
+              attributeName="transform" type="translate"
+              values="19; 19 0; 0 0"
+              dur={DURATION_TIME} />
+            <g>
+              <mask id={`${audioClass}-mask`}>
+                <polygon fill="white" points="0,0 103.4,60 0,120" />
+              </mask>
+              <circle className={cx(`${audioClass}-ring`)} cx="34.64" cy="60" r="69.2" fill={iconColor} mask={`url(#${audioClass}-mask)`}>
+                <animate attributeType="XML"
+                  attributeName="r"
+                  values="25.7; 25.7; 69.2"
+                  dur={DURATION_TIME} />
+              </circle>
+
+            </g>
+          </g>
+          <rect className={cx(`${audioClass}-eye`)} x="0" y="8.6" rx="51.7" ry="51.7" width="103.4" height="103.4" fill={iconColor} transform="scale(0.3)">
+            <animate attributeType="XML"
+              attributeName="rx"
+              values="15;51.7;51.7"
+              dur={DURATION_TIME}  />
+            <animate attributeType="XML"
+              attributeName="ry"
+              values="15;51.7;51.7"
+              dur={DURATION_TIME} />
+            <animateTransform attributeType="XML"
+              attributeName="transform" type="scale"
+              values="1; 0.5; 0.3"
+              dur={DURATION_TIME} />
+          </rect>
+        </svg>
+      </div>
+    )
+  }
+
   render() {
     let { skin, style } = this.props
     let iconColor = SKIN[skin] || skin
@@ -49,43 +134,7 @@ export default class LogoLoading extends Component {
     const toPaused = status !== AUDIO_STATUS.PLAYING
     return (
       <div className={cx(`${audioClass}`)} style={style} onClick={this.changeStatus}>
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120" className={cx(`${audioClass}-btn`, {
-          [`${audioClass}-play`]: status !== AUDIO_STATUS.PLAYING,
-          [`${audioClass}-pause`]: status === AUDIO_STATUS.PLAYING
-        })}>
-          <g className={cx(`${audioClass}-group`)} transform="translate(19, 0)">
-          <animateTransform attributeType="XML"
-            attributeName="transform" type="translate"
-            values="0 0; 19 0; 19 0"
-            dur={DURATION_TIME} />   
-            <g>
-              <mask id={`${audioClass}-mask`}>
-                <polygon fill="white" points="0,0 103.4,60 0,120" />
-              </mask>
-              <circle className={cx(`${audioClass}-ring`)} cx="34.64" cy="60" r="25.7" fill="red" mask={`url(#${audioClass}-mask)`}>
-                <animate attributeType="XML"
-                  attributeName="r"
-                  values="69.2; 25.7; 25.7"
-                  dur={DURATION_TIME} /> 
-              </circle>
-              
-            </g>
-          </g>
-          <rect className={cx(`${audioClass}-eye`)} x="0" y="8.6" rx="15" ry="15" width="103.4" height="103.4" fill="blue" opacity="0.5">
-            <animate attributeType="XML"
-              attributeName="rx"
-              values="51.7;51.7;15" 
-              dur={DURATION_TIME} style={{ transform: 'scale(1)' }} />
-            <animate attributeType="XML"
-              attributeName="ry"
-              values="51.7;51.7;15" 
-              dur={DURATION_TIME} />
-            <animateTransform attributeType="XML"
-              attributeName="transform" type="scale"
-              values="0.3; 0.5; 1"
-              dur={DURATION_TIME} />
-          </rect>
-        </svg>
+        {status === AUDIO_STATUS.PLAYING ? this.renderToPause() : this.renderToPlay() }
       </div>
     )
   }
