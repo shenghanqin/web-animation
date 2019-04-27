f73c8efface7835f485bab5d3f89d75d023615d7
shenghanqin <shenghanqin@163.com>
2019-04-27 18:31:17 +0800

    提交一版本正方向

--- stories/play-icon/examples/PlayIcon.js
+++ stories/play-icon/examples/PlayIcon.js
@@ -4,6 +4,7 @@ import PlayIconBorder from '@components/play-icon-1-border'
 import PlayIconSvg from '@components/play-icon-2-svg'
 import PlayIconIqiyi from '@components/play-icon-3-iqiyi'
 import PlayIcon4CircleRect from '@components/play-icon-4-circle-rect'
+import PlayIcon4CircleRectInner from '@components/play-icon-4-circle-rect-inner'
 
 export default class BackTopExample extends Component {
 
@@ -18,12 +19,14 @@ export default class BackTopExample extends Component {
               <th style={{ padding: '0 20px' }}><h3>V2 SVG path</h3></th>
               <th style={{ padding: '0 20px' }}><h3>V3 iqiyi path</h3></th>
               <th style={{ padding: '0 20px' }}><h3>V4 方圆三角形</h3></th>
+              <th style={{ padding: '0 20px' }}><h3>V4.5 方圆三角形 降级写法</h3></th>
             </tr>
             <tr>
               <td style={{ padding: '0 20px' }}><PlayIconBorder {...this.props} /></td>
               <td style={{ padding: '0 20px' }}><PlayIconSvg {...this.props} /></td>
               <td style={{ padding: '0 20px' }}><PlayIconIqiyi {...this.props} /></td>
               <td style={{ padding: '0 20px' }}><PlayIcon4CircleRect {...this.props} /></td>
+              <td style={{ padding: '0 20px' }}><PlayIcon4CircleRectInner {...this.props} /></td>
             </tr>
           </table>
         </React.StrictMode>
