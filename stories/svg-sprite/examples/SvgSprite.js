1ac19f8b6f871f22c9b4a9ac482ce422f761f660
shenghanqin <shenghanqin@163.com>
2019-05-17 16:50:36 +0800

    尝试使用图标情况

--- stories/svg-sprite/examples/SvgSprite.js
+++ stories/svg-sprite/examples/SvgSprite.js
@@ -14,8 +14,11 @@ export default class BackTopExample extends Component {
     return (
       <div>
         <React.StrictMode>
+          <div className={cx('side-by-side')}>
+            <svg width="240" height="240"><use xlinkHref="#ui-svg-sprite-2-all-down-circle"/></svg>
+            <svg className={cx('svg-sprite-2-story-2')} width="240" height="240"><use xlinkHref="#ui-svg-sprite-2-all-down-circle"/></svg>
+          </div>
           <SvgSprite2All></SvgSprite2All>
-          <svg width="12" height="12"><use xlinkHref="#ui-svg-sprite-2-all-down-circle"/></svg>
         </React.StrictMode>
       </div>
     )
