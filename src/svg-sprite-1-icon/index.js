8e23c371da0d52a9e5739a59ff01b4d6affae37e
shenghanqin <shenghanqin@163.com>
2019-05-17 17:33:25 +0800

    设置图标icon效果

--- src/svg-sprite-1-icon/index.js
+++ src/svg-sprite-1-icon/index.js
@@ -9,5 +9,29 @@ const prefixClass = `${PREFIX}-css-var-1-rating`
 const cx = require('classnames/bind').bind(styles)
 
 export default class CssVar1Rating extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      iconStr: ''
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      iconStr: this.getIcon('svg-sprite')
+    })
+    // console.log('getIcon :', 
+    // console.log('123 :', 123);
+  }
 
+  getIcon = (name) => {
+    console.log('name :', name);
+    return require(`@svg-icons/${name}.svg`)
+  }
+  render() {
+    const { iconStr } = this.state
+    return (
+      <svg style={{ display: 'none' }} dangerouslySetInnerHTML={{ __html: iconStr }}>
+      </svg>
+    )
+  }
 }
